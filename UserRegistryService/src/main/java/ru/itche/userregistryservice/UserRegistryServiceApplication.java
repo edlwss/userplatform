71be8b54e143bc6c9dbdb90d090cf7bef8be6290
edlws <asphodeldel@yandex.ru>
2026-03-17 08:52:15 +0300

    create structure project

---
+++ UserRegistryService/src/main/java/ru/itche/userregistryservice/UserRegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.itche.userregistryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRegistryServiceApplication.class, args);
+    }
+
+}
---
+++ UserRegistryService/src/test/java/ru/itche/userregistryservice/UserRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itche.userregistryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
